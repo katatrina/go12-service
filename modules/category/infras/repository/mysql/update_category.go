ebb2d120f19d5d6f9e0a5dd4a81d7881ca20434f
katatrina <cvphuoc2014@gmail.com>
2025-06-23 11:24:03 +0700

    refactor api update category by id

--- modules/category/infras/repository/mysql/update_category.go
+++ modules/category/infras/repository/mysql/update_category.go
@@ -2,23 +2,14 @@ package repository
 
 import (
 	"context"
-
+	
 	"github.com/google/uuid"
 	"github.com/katatrina/go12-service/modules/category/internal/model"
 )
 
 func (repo *CategoryRepository) Update(ctx context.Context, id uuid.UUID, dto *model.UpdateCategoryDTO) error {
-	db := repo.db.Begin()
-
-	if err := repo.db.Model(&model.Category{}).Where("id = ?", id).Updates(dto).Error; err != nil {
-		db.Rollback()
-		return err
-	}
-
-	if err := db.Commit().Error; err != nil {
-		db.Rollback()
-		return err
-	}
-
-	return nil
+	return repo.db.WithContext(ctx).
+		Model(&model.Category{}).
+		Where("id = ?", id).
+		Updates(dto).Error
 }
